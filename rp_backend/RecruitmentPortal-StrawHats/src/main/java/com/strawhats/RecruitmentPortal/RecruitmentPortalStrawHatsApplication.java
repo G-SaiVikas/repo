e43d2abd103af7c02010dd5712c4710607af1d91
G-SaiVikas <saivikas2308@gmail.com>
2024-05-31 16:52:06 -0500

    intial commit

---
+++ rp_backend/RecruitmentPortal-StrawHats/src/main/java/com/strawhats/RecruitmentPortal/RecruitmentPortalStrawHatsApplication.java
@@ -0,0 +1,13 @@
+package com.strawhats.RecruitmentPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentPortalStrawHatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentPortalStrawHatsApplication.class, args);
+	}
+
+}
