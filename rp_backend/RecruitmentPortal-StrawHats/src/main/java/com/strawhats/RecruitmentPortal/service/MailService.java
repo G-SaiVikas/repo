74a2ff880211328245ed8e3d7d1f998059aef2ed
LankaRohith <rohit.lnk33@gmail.com>
2024-06-03 11:06:49 -0500

    #RP3-Added application properties of spring mail and added spring mail dependency

--- rp_backend/RecruitmentPortal-StrawHats/src/main/java/com/strawhats/RecruitmentPortal/service/MailService.java
+++ rp_backend/RecruitmentPortal-StrawHats/src/main/java/com/strawhats/RecruitmentPortal/service/MailService.java
@@ -1,10 +1,12 @@
 package com.strawhats.RecruitmentPortal.service;
 
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
 import com.strawhats.RecruitmentPortal.model.MailStructure;
 
 @Service
 public class MailService {
-	public void sendMail(String mail, MailStructure mailStructure) { }
+	public void sendMail(String mail, MailStructure mailStructure) { 
+	}
 }
