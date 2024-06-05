a125bc079d2a6908e1fc4f11ca728d1523c1c66f
LankaRohith <rohit.lnk33@gmail.com>
2024-06-05 10:34:01 -0500

    RP4-Created getjobs API at controller layer

--- rp_backend/RecruitmentPortal-StrawHats/src/main/java/com/strawhats/RecruitmentPortal/controller/JobController.java
+++ rp_backend/RecruitmentPortal-StrawHats/src/main/java/com/strawhats/RecruitmentPortal/controller/JobController.java
@@ -1,5 +1,25 @@
 package com.strawhats.RecruitmentPortal.controller;
 
-public class JobController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.strawhats.RecruitmentPortal.model.Job;
+import com.strawhats.RecruitmentPortal.service.JobService;
+
+@RestController
+@RequestMapping("/jobs")
+public class JobController {
+	
+	@Autowired
+    private JobService jobService;
+	
+	@GetMapping("/getjobs")
+	public List<Job> getAllJobs() {
+		return jobService.getAllJobs();
+	}
+	
 }
