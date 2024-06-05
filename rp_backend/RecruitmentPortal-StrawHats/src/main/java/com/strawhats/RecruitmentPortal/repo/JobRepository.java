865d706a823a487279c881087a62820e493f1758
LankaRohith <rohit.lnk33@gmail.com>
2024-06-05 10:37:31 -0500

    RP4-Added JobRepository interface and a finder method for data JPA

---
+++ rp_backend/RecruitmentPortal-StrawHats/src/main/java/com/strawhats/RecruitmentPortal/repo/JobRepository.java
@@ -0,0 +1,14 @@
+package com.strawhats.RecruitmentPortal.repo;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.strawhats.RecruitmentPortal.model.Job;
+import com.strawhats.RecruitmentPortal.model.User;
+
+@Repository
+public interface JobRepository extends JpaRepository<Job, Long> {
+	List<Job> findByApplicants(User user);
+}
