50d34709329fd2829b8e3ed33c1e485679a27e4b
saidugyala <saitejarao128@gmail.com>
2024-06-06 23:52:07 -0500

    RP6-Added many to many association from user entity to job entity

--- rp_backend/RecruitmentPortal-StrawHats/src/main/java/com/strawhats/RecruitmentPortal/model/User.java
+++ rp_backend/RecruitmentPortal-StrawHats/src/main/java/com/strawhats/RecruitmentPortal/model/User.java
@@ -1,8 +1,11 @@
 package com.strawhats.RecruitmentPortal.model;
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,5 +23,8 @@ public class User {
     private String email;
     private String password;
     private String phoneNumber;
+    
+    @ManyToMany(mappedBy = "applicants")
+    private List<Job> appliedJobs;
     
 }
