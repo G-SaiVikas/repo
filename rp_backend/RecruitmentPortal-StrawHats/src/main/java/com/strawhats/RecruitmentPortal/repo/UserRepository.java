b316a7412adeb0ef4f3021103f6ac18fba45bcec
saidugyala <saitejarao128@gmail.com>
2024-06-06 23:55:04 -0500

    RP6-Added finder method to user repo to find by applicants

--- rp_backend/RecruitmentPortal-StrawHats/src/main/java/com/strawhats/RecruitmentPortal/repo/UserRepository.java
+++ rp_backend/RecruitmentPortal-StrawHats/src/main/java/com/strawhats/RecruitmentPortal/repo/UserRepository.java
@@ -1,10 +1,12 @@
 package com.strawhats.RecruitmentPortal.repo;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.strawhats.RecruitmentPortal.model.Job;
 import com.strawhats.RecruitmentPortal.model.User;
 
 
@@ -13,6 +15,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
     User findByFullNameAndPhoneNumber(String fullName, String phoneNumber);
+    List<Job> findByApplicants(User user);
 }
 
 
